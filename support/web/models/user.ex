c6424f7b852efa63d861612e8c23c3352cd79130
Tejas Manohar <me@tejas.io>
2015-09-21 19:35:53 -0500

    generate models w/ mix and setup virtuals

---
+++ support/web/models/user.ex
@@ -0,0 +1,28 @@
+defmodule Support.User do
+  use Support.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :encrypted_password, :string
+
+    field :password, :string, :virtual true
+    field :password_confirmation, :string, :virtual true
+
+    timestamps
+  end
+
+  @required_fields ~w(name email encrypted_password)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
